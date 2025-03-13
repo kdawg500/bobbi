fadad7cce19b9b814f44a9850a1d68bad68fc358
Khizar Iqbal <khizar.iqbal@clearroute.io>
2025-03-13 12:02:59 +0000

    adding test

--- link/link_test.go
+++ link/link_test.go
@@ -1,7 +1,14 @@
 package link
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestValidate(t *testing.T) {
-
+	linkString := "facebook.com"
+	expected := "https://facebook.com"
+	actual := Validate(linkString)
+	assert.Equal(t, expected, actual)
 }
